4db8f8b806d0b90ad9e043432231d143c71f6802
marcelodavid <marcelo.arevalos@hotmail.com>
2015-09-07 17:36:28 -0400

    se agrega las operaciones resumen_de_abonado y resumen_de_zonas

--- model/historial.js
+++ model/historial.js
@@ -1,16 +1,37 @@
 var assert = require('assert');
 
-var nueva_entrada = function(db, data, callback){
+var abonado = function(db, data){
 	var collection = db.collection('historial');
 	
+	collection.ensureIndex({fecha: -1, abonado_id: 1}, function(err, success){
+		assert.equal(err, null, ['error al indexar los atributos']);
+	});
+
 	collection.insert(data, function(err, success){
 		assert.equal(err, null, ['error al insertar los datos']);
 		assert.equal(success.result.n, 1);
-	});
-
-	collection.ensureIndex({time: -1, home_id: 1}, function(err, success){
-		assert.equal(err, null, ['error al indexar los atributos']);
+		db.close();
 	});
 }
 
-exports.nueva_entrada = nueva_entrada;
+var resumen_de_abonado = function(db, id, fecha, callback){
+	var collection = db.collection('historial');
+
+	collection.find({abonado_id: id, fecha:{$gt: fecha}}, {_id:0}).toArray(function(err, docs){
+		callback(docs);
+		db.close();
+	});
+}
+
+var resumen_de_zona = function(db, id, fecha, callback){
+	var collection = db.collection('historial');
+
+	collection.find({zona_id:id, fecha:{$gt:fecha}}, {_id:0}).toArray(function(err, docs){
+		callback(docs);
+		db.close();
+	});
+}
+
+exports.abonado = abonado;
+exports.resumen_de_abonado = resumen_de_abonado;
+exports.resumen_de_zona = resumen_de_zona;
